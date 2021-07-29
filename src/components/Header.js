6c8f1073c488687361f5f9b10567efa256bf4fbe
AvinashKumarMahato <akr34206@gmail.com>
2021-07-29 11:41:59 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+const Header = props => {
+    const { search, onInputChange, onSearchClick } = props;
+    return (
+        <div className="jumbotron">
+            <h1 className="display-1">
+                <i class="material-icons brand-icon">fastfood </i>Food Recipe</h1>
+
+            <div className="input-group w-50 mx-auto">
+                <input type="text" className="form-control" placeholder="Search Your Favourite Recipe" value={search}
+                    onChange={onInputChange}
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-dark" onClick={onSearchClick}>Search Recipe</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Header;
